51855e586afeebe97a6fca86203ab40bde681811
stormckey <sortygraph@gmail.com>
2024-03-19 08:21:46 +0000

    3/karel done

--- assign3/program/src/karel_impl.ml
+++ assign3/program/src/karel_impl.ml
@@ -38,7 +38,23 @@ let set_cell (grid : grid) ((i, j) : pos) (cell : cell) : grid =
 ;;
 
 let state_to_string (state : state) : string =
-  raise Unimplemented
+  let string_matrix = List.map state.grid ~f:(fun row -> 
+    List.map row ~f:(function 
+      | Empty -> "."
+      | Wall -> "X"
+      | Beeper -> "B")) in
+  let (x, y) = state.karel_pos in
+  let karel_matrix = List.mapi string_matrix ~f:(fun j row -> 
+    if j <> y then row else
+      List.mapi row ~f:(fun i cell -> 
+        if i <> x then cell else
+          match cell with 
+          | "." | "B" -> "K"
+          | "X" -> failwith "K in the wall"
+          | _ -> failwith "other char in string_matrix"
+      )) in
+  List.map karel_matrix ~f:(fun row -> String.concat ~sep:" " row)
+  |> String.concat ~sep:"\n"
 ;;
 
 let empty_grid (m : int) (n : int) : grid =
@@ -94,12 +110,55 @@ and instruction_list_to_string (instrs: instruction list) : string =
   String.concat ~sep:", " (List.map ~f:instruction_to_string instrs)
 
 
+let new_pos state =
+  let (x, y) = state.karel_pos in
+  let (dx, dy) = match state.karel_dir with
+    | North -> (0, -1)
+    | West -> (-1, 0)
+    | South -> (0, 1)
+    | East -> (1, 0) in
+  (x + dx, y + dy)
+  
 let rec eval_pred (state : state) (pred : predicate) : bool =
-  raise Unimplemented
+    match pred with 
+    | Not p -> not (eval_pred state p)
+    | Facing dir -> state.karel_dir = dir
+    | NoBeepersPresent ->  not (get_cell state.grid state.karel_pos = Some Beeper)
+    | FrontIs cell -> 
+      match get_cell state.grid (new_pos state) with 
+      | None -> cell = Wall
+      | Some c -> c = cell
 
 
 let rec step (state : state) (code : instruction) : state =
-  raise Unimplemented
+  match code with 
+  | Move ->
+    if eval_pred state (FrontIs Wall)
+    then 
+      state
+    else 
+      {state with karel_pos = new_pos state}
+  | TurnLeft ->
+    {state with karel_dir = match state.karel_dir with
+    | North -> West
+    | West -> South
+    | South -> East
+    | East -> North}
+  | PickBeeper ->
+     (match get_cell state.grid state.karel_pos with
+      | Some Beeper -> 
+        {state with grid = set_cell state.grid state.karel_pos Empty}
+      | _ -> state)
+  | PutBeeper ->
+    {state with grid = set_cell state.grid state.karel_pos Beeper}
+  | While (pred, insts) ->
+    if eval_pred state pred
+    then step (step_list state insts) code
+    else state
+  | If (pred, then_, else_) ->
+    if eval_pred state pred
+    then step_list state then_
+    else step_list state else_
 
 and step_list (state : state) (instrs : instruction list) : state =
   List.fold instrs ~init:state ~f:(fun state instr ->
@@ -116,5 +175,34 @@ and step_list (state : state) (instrs : instruction list) : state =
 
 ;;
 
-let checkers_algo : instruction list = [
+let not_wall = Not (FrontIs Wall);;
+let check_put = If (not_wall, [Move;PutBeeper], []);;
+
+let to_end = While (not_wall, [Move;check_put])
+
+let checkers_algo=
+  [
+    PutBeeper;
+    to_end;
+    TurnLeft;
+    TurnLeft;
+    TurnLeft;
+    While (not_wall, [
+      Move; 
+      TurnLeft; 
+      TurnLeft; 
+      TurnLeft; 
+      Move;
+      PutBeeper;
+      to_end;
+      TurnLeft;
+      If (not_wall, [
+        Move; 
+        TurnLeft;
+        PutBeeper; 
+        to_end], []);
+      TurnLeft;
+      TurnLeft;
+      TurnLeft;
+    ])
 ]
